20415d80267fdc8e546d4d4d17b3719ef67fbc5b
Mario Jimenez <majimenez@irtacasa.es>
2025-06-16 12:15:41 +0200

    Add kafka

---
+++ docker/kafka/init-scripts/create-topics.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Esperamos unos segundos a que el broker arranque
+sleep 10
+
+echo "✅ Creando topic music-recommendation-features si no existe..."
+
+kafka-topics --bootstrap-server broker:29092 \
+  --create --if-not-exists \
+  --topic music-recommendation-features \
+  --partitions 1 \
+  --replication-factor 1
+
+echo "✅ Topic creado"
