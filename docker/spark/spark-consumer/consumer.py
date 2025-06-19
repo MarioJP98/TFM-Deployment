a3097f1ad925b0295a9d81c3e84af786077a7e03
Mario Jimenez <majimenez@irtacasa.es>
2025-06-19 10:27:57 +0200

    Add new topic and send the result to a kafka queue

--- docker/spark/spark-consumer/consumer.py
+++ docker/spark/spark-consumer/consumer.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, to_json, struct
 from pyspark.sql.types import StructType, StructField, DoubleType, StringType
 from pyspark.ml import PipelineModel
 
@@ -35,8 +35,7 @@ df = spark.readStream \
 
 # Convertir de binario a string y parsear JSON
 df = df.selectExpr("CAST(value AS STRING) as json_str")
-parsed_df = df.select(
-    from_json(col("json_str"), schema).alias("data")).select("data.*")
+parsed_df = df.select(from_json(col("json_str"), schema).alias("data")).select("data.*")
 
 # Cargar modelo entrenado
 model = PipelineModel.load("/app/models/music-recommender-model")
@@ -46,8 +45,6 @@ clustered_songs = spark.read.csv(
     "/app/data/songs_with_cluster.csv", header=True, inferSchema=True)
 
 # Función de inferencia por batch y recomendación
-
-
 def process_batch(batch_df, batch_id):
     try:
         print(f"\n--- Batch {batch_id} ---")
@@ -58,18 +55,29 @@ def process_batch(batch_df, batch_id):
             predictions = model.transform(batch_df)
             predictions.select("track_name", "cluster_id").show()
 
-            predicted_cluster = predictions.select(
-                "cluster_id").first()["cluster_id"]
+            predicted_cluster = predictions.select("cluster_id").first()["cluster_id"]
 
             # Buscar canciones similares
             recommendations = clustered_songs.filter(
-                col("cluster_id") == predicted_cluster)
+                col("cluster_id") == predicted_cluster
+            ).select("artist", "title").limit(3)
 
-            print("\n Recomendaciones similares:")
-            recommendations.select("artist", "title").limit(3).show()
+            # Mostrar por consola
+            print("\Recomendaciones similares:")
+            recommendations.show()
+
+            # Enviar recomendaciones a Kafka
+            kafka_ready = recommendations.withColumn("value", to_json(struct("artist", "title"))) \
+                                         .selectExpr("CAST(value AS STRING)")
+
+            kafka_ready.write \
+                .format("kafka") \
+                .option("kafka.bootstrap.servers", "broker:29092") \
+                .option("topic", "music-recommendation-result") \
+                .save()
+
     except Exception as e:
         print(f"[ERROR en batch {batch_id}]: {e}")
-
 
 # Iniciar el stream
 query = parsed_df.writeStream \
